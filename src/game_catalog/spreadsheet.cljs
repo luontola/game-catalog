c5065faef34cadcb519971a0139505044fe941c2
Esko Luontola <esko@luontola.fi>
2022-05-20 20:37:53 +0300

    Spreadsheet editor spike

--- src/game_catalog/spreadsheet.cljs
+++ src/game_catalog/spreadsheet.cljs
@@ -6,27 +6,44 @@
   (str/join ", " value))
 
 (defn data-cell [{:keys [*data data-path data-type reference-path]}]
-  (r/with-let [*self (r/atom nil)]
+  (r/with-let [*self (r/atom nil)
+               *editing? (r/atom false)]
     (let [value (get-in @*data data-path)]
-      [:td {:tab-index 0
+      [:td {:tab-index (if @*editing? -1 0)
             :ref #(reset! *self %)
             :on-double-click (fn [event]
-                               (js/console.log event))}
-       [:div.data-cell {:style (when (= :money data-type)
-                                 {:text-align "right"})}
-        (case data-type
-          :multi-select (format-multi-select-value value)
-          :reference (->> value
-                          (map (fn [id]
-                                 (if-some [record (get-in @*data (conj reference-path id))]
-                                   ;; TODO: the record should determine itself that how to format it
-                                   (case (first reference-path)
-                                     :games (str (:name record))
-                                     :purchases (format-multi-select-value (:shop record))
-                                     (str record))
-                                   (str id))))
-                          (str/join "; "))
-          (str value))]])))
+                               (js/console.log event)
+                               (reset! *editing? true))}
+       (if @*editing?
+         [:input {:type "text"
+                  :auto-focus true
+                  :on-blur (fn [event]
+                             (js/console.log event)
+                             (reset! *editing? false))
+                  :style {:width "100%"
+                          :height "36px"
+                          :border 0
+                          :margin-right "-1px"
+                          :padding "6px 5px 6px 6px"
+                          :background-color "transparent"
+                          :outline "3px solid #4884f9"
+                          :outline-offset "-1px"}}]
+
+         [:div.data-cell {:style (when (= :money data-type)
+                                   {:text-align "right"})}
+          (case data-type
+            :multi-select (format-multi-select-value value)
+            :reference (->> value
+                            (map (fn [id]
+                                   (if-some [record (get-in @*data (conj reference-path id))]
+                                     ;; TODO: the record should determine itself that how to format it
+                                     (case (first reference-path)
+                                       :games (str (:name record))
+                                       :purchases (format-multi-select-value (:shop record))
+                                       (str record))
+                                     (str id))))
+                            (str/join "; "))
+            (str value))])])))
 
 (defn table [{:keys [*data data-path sort-key columns]}]
   (let [records (->> (get-in @*data data-path)
