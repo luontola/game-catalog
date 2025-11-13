e13f07f4e484548456ebda11e0fafff8088be455
Esko Luontola <esko@luontola.fi>
2025-11-13 12:36:48 +0200

    Default styling

--- src/game_catalog/ui/layout.clj
+++ src/game_catalog/ui/layout.clj
@@ -13,6 +13,8 @@
            [:title {} "Game Catalog"]
            [:meta {:name "viewport"
                    :content "width=device-width, initial-scale=1"}]
+           [:link {:rel "stylesheet" :href "/reset.css"}]
+           [:link {:rel "stylesheet" :href "/styles.css"}]
            [:script {:src "https://cdn.jsdelivr.net/npm/htmx.org@4.0.0-alpha2/dist/htmx.min.js"}]
            [:script {:src "/scripts.js"}]]
           [:body {:hx-headers:inherited (html/anti-forgery-headers-json)}
