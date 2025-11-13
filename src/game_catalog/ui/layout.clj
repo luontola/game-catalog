c53e41dd9cc77f95917bfa99b28f004d678e1c2a
Esko Luontola <esko@luontola.fi>
2025-11-13 12:37:33 +0200

    Simple.css

--- src/game_catalog/ui/layout.clj
+++ src/game_catalog/ui/layout.clj
@@ -14,6 +14,7 @@
            [:meta {:name "viewport"
                    :content "width=device-width, initial-scale=1"}]
            [:link {:rel "stylesheet" :href "/reset.css"}]
+           [:link {:rel "stylesheet" :href "/simple.css"}]
            [:link {:rel "stylesheet" :href "/styles.css"}]
            [:script {:src "https://cdn.jsdelivr.net/npm/htmx.org@4.0.0-alpha2/dist/htmx.min.js"}]
            [:script {:src "/scripts.js"}]]
