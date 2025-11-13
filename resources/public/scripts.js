0d0cdf4a055e4f893d9e79dfc147c3c3e0e4dea6
Esko Luontola <esko@luontola.fi>
2025-11-13 13:43:11 +0200

    Arrow key navigation left and right

--- resources/public/scripts.js
+++ resources/public/scripts.js
@@ -1,1 +1,19 @@
 htmx.config.transitions = false
+
+// Spreadsheet arrow key navigation
+document.addEventListener('keydown', (e) => {
+    const cell = e.target
+    if (!cell.matches('.spreadsheet td')) {
+        return
+    }
+    let targetCell = null
+    if (e.key === 'ArrowLeft') {
+        targetCell = cell.previousElementSibling
+    } else if (e.key === 'ArrowRight') {
+        targetCell = cell.nextElementSibling
+    }
+    if (targetCell) {
+        e.preventDefault()
+        targetCell.focus()
+    }
+})
