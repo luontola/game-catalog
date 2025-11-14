74b56e25b6f2f535a13b4faa9f4ec398f6d3ecbe
Esko Luontola <esko@luontola.fi>
2025-11-14 11:00:07 +0200

    Fix Cursive IDE false warning about the thrown? macro

---
+++ src/game_catalog/infra/testutil.clj
@@ -0,0 +1,8 @@
+(ns game-catalog.infra.testutil
+  (:require [clojure.test :refer :all]))
+
+;; these can be required to avoid IDE warnings about the built-in clojure.test/is macro special forms
+(declare ^{:arglists '([exception-class body])}
+         thrown?)
+(declare ^{:arglists '([exception-class regex body])}
+         thrown-with-msg?)
