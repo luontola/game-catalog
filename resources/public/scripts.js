6d5abef9e8f22f00e8e51a762057f767f26f816d
Esko Luontola <esko@luontola.fi>
2025-11-14 16:51:09 +0200

    Refactor

--- resources/public/scripts.js
+++ resources/public/scripts.js
@@ -14,16 +14,6 @@ document.addEventListener('input', (e) => {
     }
 })
 
-// Scroll to bottom when the adding row receives focus
-document.addEventListener('focusin', (e) => {
-    if (e.target.closest('.spreadsheet tr.adding')) {
-        window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'instant'
-        })
-    }
-})
-
 function isFormModified(row) {
     return row.dataset.modified === 'true'
 }
@@ -194,18 +184,6 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
-// Avoid multiple autofocus attributes in the DOM after htmx swaps.
-// Otherwise, the first autofocus in the DOM always grabs the focus.
-document.addEventListener('focusin', e => {
-    if (e.target.matches('input') && e.target.hasAttribute('autofocus')) {
-        // When autofocus focuses a text input element, the cursor will be
-        // in the front of any existing text. We want to select all the text.
-        // Then the user can easily replace it or press the right arrow to append text.
-        e.target.select()
-    }
-    e.target.removeAttribute('autofocus')
-})
-
 // Exit edit mode when focus leaves the row
 document.addEventListener('focusout', (e) => {
     const row = e.target.closest('.spreadsheet tr.editing')
@@ -237,6 +215,29 @@ document.addEventListener('dblclick', (e) => {
         return
     }
     enterEditMode(row, cell)
+})
+
+// Scroll to bottom when the adding row receives focus
+document.addEventListener('focusin', (e) => {
+    if (e.target.closest('.spreadsheet tr.adding')) {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'instant'
+        })
+    }
+})
+
+// Avoid multiple autofocus attributes in the DOM after htmx swaps.
+// Otherwise, the first autofocus in the DOM always grabs the focus.
+document.addEventListener('focusin', e => {
+    const node = e.target;
+    if (node.matches('input') && node.hasAttribute('autofocus')) {
+        // When autofocus focuses a text input element, the cursor will be
+        // in the front of any existing text. We want to select all the text.
+        // Then the user can easily replace it or press the right arrow to append text.
+        node.select()
+        node.removeAttribute('autofocus')
+    }
 })
 
 // Watch for elements with auto-scroll-into-view and scroll them into view
