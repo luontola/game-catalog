28f08b369818b9b29f2ad0ad536b3b1a56a4d697
Esko Luontola <esko@luontola.fi>
2026-05-22 11:53:37 +0300

    Refactor

--- test/game_catalog/ui/spreadsheet_render_test.clj
+++ test/game_catalog/ui/spreadsheet_render_test.clj
@@ -4,7 +4,7 @@
             [game-catalog.ui.spreadsheet :as spreadsheet]
             [game-catalog.ui.spreadsheet.numeric :as numeric]))
 
-(def things-config
+(def config
   {:collection-key :things
    :columns [{:column/name "Texty"
               :column/entity-key :thing/texty}
@@ -12,22 +12,21 @@
                :column/name "Numbery"
                :column/entity-key :thing/numbery)]})
 
+(def entity
+  {:entity/id "1"
+   :thing/texty "Words"
+   :thing/numbery 123})
+
 (deftest view-row-test
   (testing "renders column type classes on cells"
     (let [doc (html/parse-table-fragment
-                (spreadsheet/view-row things-config
-                                      {:entity/id "1"
-                                       :thing/texty "Words"
-                                       :thing/numbery 123}))]
+                (spreadsheet/view-row config entity))]
       (is (some? (.selectFirst doc "td.column-text")))
       (is (some? (.selectFirst doc "td.column-numeric"))))))
 
 (deftest edit-row-test
   (testing "renders column type classes on editable cells"
     (let [doc (html/parse-table-fragment
-                (spreadsheet/edit-row things-config
-                                      {:entity/id "1"
-                                       :thing/texty "Words"
-                                       :thing/numbery 123}))]
+                (spreadsheet/edit-row config entity))]
       (is (some? (.selectFirst doc "td.column-text")))
       (is (some? (.selectFirst doc "td.column-numeric"))))))
